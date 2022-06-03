3aab30722c884f189abda60ced8ddaec793ebfb5
michaelessiet <emsaa2002@gmail.com>
2022-06-03 23:01:51 +0800

    worked on the improvements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import activities from "./resources/schedule.json";
 import dayjs from "dayjs";
-import isoWeek from "dayjs/plugin/isoWeek";
 import { useEffect } from "react";
 import { useState } from "react";
 
+function SortedActivityListComponent({ activityList, title }) {
+  if (activityList.length !== 0) {
+    return (
+      <div>
+        <h3>{title}</h3>
+        <ol className="list-unstyled">
+          {activityList.map((activity) => {
+            return (
+              <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
+                <header>{activity.title}</header>
+                <small className="text-muted">
+                  {dayjs(activity.startTime).format("D MMM [at} H:mm") >
+                  dayjs().add(14, "day").format("D MMM [at]H:mm")
+                    ? dayjs(activity.startTime).format("ddd[,] D MMM [at] H:mm")
+                    : dayjs(activity.startTime).format("D MMM [at] H:mm")}{" "}
+                  • {activity.instructor}
+                </small>
+              </li>
+            );
+          })}
+        </ol>
+        <hr />
+      </div>
+    );
+  } else return;
+}
+
 function App() {
-  const currentDate = dayjs().format("DD MMMM");
-  const tomorrowsDate = dayjs().add(1, "day").format("DD MMMM");
-  dayjs.extend(isoWeek);
+  const currentDate = dayjs().format("DD MMMM YYYY");
+  const tomorrowsDate = dayjs().add(1, "day").format("DD MMMM YYYY");
 
   const [todaysActivityList, setTodaysActivityList] = useState([]);
   const [tomorrowsActivityList, setTomorrowsActivityList] = useState([]);
@@ -15,188 +40,48 @@ function App() {
   const [nextWeeksActivityList, setNextWeeksActivityList] = useState([]);
   const [futureActivityList, setFutureActivityList] = useState([]);
 
-  function todaysActivities() {
-    const activityGroup = activities.filter(
-      (activity) => dayjs(activity.startTime).format("DD MMMM") === currentDate
-    );
-    setTodaysActivityList(activityGroup);
-  }
-
-  function tomorrowsActivities() {
-    const activityGroup = activities.filter(
+  function sortActivitiesIntoGroups() {
+    const todaysActivities = activities.filter(
       (activity) =>
-        dayjs(activity.startTime).format("DD MMMM") === tomorrowsDate
+        dayjs(activity.startTime).format("DD MMMM YYYY") === currentDate
     );
-    setTomorrowsActivityList(activityGroup);
-  }
-
-  function restOfWeekActivities() {
-    const activityGroup = activities.filter(
+    const tomorrowsActivities = activities.filter(
       (activity) =>
-        dayjs(activity.startTime).format("DD MMMM") !== currentDate &&
-        dayjs(activity.startTime).format("DD MMMM") !== tomorrowsDate &&
-        dayjs(activity.startTime).format("DD MMMM") >=
-          dayjs().day(0).format("DD MMMM") &&
-        dayjs(activity.startTime).format("DD MMMM") <=
-          dayjs().day(6).format("DD MMMM")
+        dayjs(activity.startTime).format("DD MMMM YYYY") === tomorrowsDate
     );
-    setRestOfWeekActivityList(activityGroup);
-  }
-
-  function nextWeeksActivities() {
-    const activityGroup = activities.filter(
+    const restOfWeekActivities = activities.filter(
       (activity) =>
-        dayjs(activity.startTime).format("DD MMMM") >
-          dayjs().day(6).format("DD MMMM") &&
-        dayjs(activity.startTime).format("DD MMMM") <
-          dayjs().day(13).format("DD MMMM")
+        dayjs(activity.startTime).format("DD MMMM YYYY") >=
+          dayjs().day(2).format("DD MMMM YYYY") &&
+        dayjs(activity.startTime).format("DD MMMM YYYY") <=
+          dayjs().day(6).format("DD MMMM YYYY")
     );
-
-    setNextWeeksActivityList(activityGroup);
-  }
-
-  function futureActivities() {
-    const activityGroup = activities.filter(
+    const nextWeeksActivities = activities.filter(
       (activity) =>
-        dayjs(activity.startTime).format("DD MMMM") >
-          dayjs().day(13).format("DD MMMM") || 
-        dayjs(activity.startTime).format("DD MMMM") <
-          dayjs().add(1, "month").format("DD MMMM") 
+        dayjs(activity.startTime).format("DD MMMM YYYY") >
+          dayjs().day(6).format("DD MMMM YYYY") &&
+        dayjs(activity.startTime).format("DD MMMM YYYY") <
+          dayjs().day(13).format("DD MMMM YYYY")
     );
-    console.log(activityGroup);
-    const futureActivityGroup = activityGroup.slice(0,5)
-    setFutureActivityList(futureActivityGroup);
-  }
+    const activityGroup = activities
+      .filter(
+        (activity) =>
+          dayjs(activity.startTime).format("DD MMMM YYYY") >
+            dayjs().day(13).format("DD MMMM YYYY") ||
+          dayjs(activity.startTime).format("DD MMMM YYYY") <
+            dayjs().add(1, "month").format("DD MMMM YYYY")
+      )
+      .slice(0, 5);
 
-  function todaysActivitiesComponent() {
-    if (todaysActivityList.length !== 0) {
-      return (
-        <div>
-          <h3>Today</h3>
-          <ol className="list-unstyled">
-            {todaysActivityList.map((activity) => {
-              return (
-                <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
-                  <header>{activity.title}</header>
-                  <small className="text-muted">
-                    {dayjs(activity.startTime).format("D MMM [at] H:mm")} •{" "}
-                    {activity.instructor}
-                  </small>
-                </li>
-              );
-            })}
-          </ol>
-          <hr />
-        </div>
-      );
-    } else return;
-  }
-
-  function tomorrowsActivitiesComponent() {
-    if (tomorrowsActivityList.length !== 0) {
-      return (
-        <div>
-          <h3>Tomorrow</h3>
-          <ol className="list-unstyled">
-            {tomorrowsActivityList.map((activity) => {
-              return (
-                <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
-                  <header>{activity.title}</header>
-                  <small className="text-muted">
-                    {dayjs(activity.startTime).format("D MMM [at] H:mm")} •{" "}
-                    {activity.instructor}
-                  </small>
-                </li>
-              );
-            })}
-          </ol>
-          <hr />
-        </div>
-      );
-    }
-  }
-
-  function restOfWeekActivitiesComponent() {
-    if (restOfWeekActivityList.length !== 0) {
-      return (
-        <div>
-          <h3>This Week</h3>
-          <ol className="list-unstyled">
-            {restOfWeekActivityList.map((activity) => {
-              return (
-                <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
-                  <header>{activity.title}</header>
-                  <small className="text-muted">
-                    {dayjs(activity.startTime).format("ddd[,] D MMM [at] H:mm")} •{" "}
-                    {activity.instructor}
-                  </small>
-                </li>
-              );
-            })}
-          </ol>
-          <hr />
-        </div>
-      );
-    }
-  }
-
-  function nextWeeksActivitiesComponent() {
-    if (nextWeeksActivityList.length !== 0) {
-      return (
-        <div>
-          <h3>Next Week</h3>
-          <ol className="list-unstyled">
-            {nextWeeksActivityList.map((activity) => {
-              return (
-                <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
-                  <header>{activity.title}</header>
-                  <small className="text-muted">
-                    {dayjs(activity.startTime).format(
-                      "ddd[,] DD MMM [at] H:mm"
-                    )}{" "}
-                    • {activity.instructor}
-                    {}
-                  </small>
-                </li>
-              );
-            })}
-          </ol>
-          <hr />
-        </div>
-      );
-    }
-  }
-
-  function futureActivitiesComponent() {
-    if (futureActivityList.length !== 0) {
-      return (
-        <div>
-          <h3>Future</h3>
-          <ol className="list-unstyled">
-            {futureActivityList.map((activity) => {
-              return (
-                <li className="hover-light-bg p-2 rounded-2" key={activity.id}>
-                  <header>{activity.title}</header>
-                  <small className="text-muted">
-                    {dayjs(activity.startTime).format("D MMM [at] H:mm")} •{" "}
-                    {activity.instructor}
-                  </small>
-                </li>
-              );
-            })}
-          </ol>
-          <hr />
-        </div>
-      );
-    }
+    setTodaysActivityList(todaysActivities);
+    setTomorrowsActivityList(tomorrowsActivities);
+    setRestOfWeekActivityList(restOfWeekActivities);
+    setNextWeeksActivityList(nextWeeksActivities);
+    setFutureActivityList(activityGroup);
   }
 
   useEffect(() => {
-    todaysActivities();
-    tomorrowsActivities();
-    restOfWeekActivities();
-    nextWeeksActivities();
-    futureActivities();
+    sortActivitiesIntoGroups();
   }, []);
 
   return (
@@ -205,12 +90,26 @@ function App() {
       <div className="row">
         <div className="col-lg-6 col-md-8">
           <h3 className="fs-6">Showing all</h3>
-
-          {todaysActivitiesComponent()}
-          {tomorrowsActivitiesComponent()}
-          {restOfWeekActivitiesComponent()}
-          {nextWeeksActivitiesComponent()}
-          {futureActivitiesComponent()}
+          <SortedActivityListComponent
+            activityList={todaysActivityList}
+            title="Today"
+          />
+          <SortedActivityListComponent
+            activityList={tomorrowsActivityList}
+            title="Tomorrow"
+          />
+          <SortedActivityListComponent
+            activityList={restOfWeekActivityList}
+            title="This week"
+          />
+          <SortedActivityListComponent
+            activityList={nextWeeksActivityList}
+            title="Next week"
+          />
+          <SortedActivityListComponent
+            activityList={futureActivityList}
+            title="In future..."
+          />
         </div>
       </div>
     </div>
